b0bd8d785d3af4194ac8899341236d7354282bd4
thomas <thomasgrz@outlook.com>
2018-10-16 01:01:55 -0400

    type: Functionality
    added modal functionality for the About section and the break/study timer alerts
    
    need to create modal showing desired save data

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,3 +1,23 @@
 console.log("modal logic connected")
 let modalbutton = document.getElementsByClassName("close")[0]
 modalbutton.onclick = ()=>{modal.style.display = "none"}
+
+let aboutmodalbutton = document.getElementsByClassName("about-modal-button")[0]
+aboutmodalbutton.setAttribute("onclick","About()")
+
+let aboutmodal = document.getElementById("about-modal")
+
+let about_close = document.getElementById("close-about-modal")
+about_close.setAttribute("onclick","closeAbout()")
+//close about modal clicks 
+function closeAbout(){
+    debugger
+    aboutmodal.style.display = "none"
+}
+
+//about modal clicks
+function About(){
+    debugger
+    let about = document.getElementById("about-modal")
+    about.style.display = "block"
+}
--- src/js/timer.js
+++ src/js/timer.js
@@ -1,3 +1,9 @@
+//about modal clicks
+function About(){
+    debugger
+    let about = document.getElementById("about-modal")
+    about.style.display = "block"
+}
 //create a start stop button with a changing
 //mode attribute
 const button = document.getElementById("button")
@@ -17,9 +23,6 @@ let fiveminutebreak, studyTimer
 //create memory of the timer cycles
 let round = 0
 
-//keep track of what timer is running
-let activeTimerType = "study"
-
 //grab the timer elements from the DOM
 let minutes = document.getElementById("minutes")
 let seconds = document.getElementById("seconds")
@@ -27,24 +30,32 @@ let seconds = document.getElementById("seconds")
 //save the time amount corresponding to seconds/minutes
 //for reference when the timer is toggled
 let memory  = {
-    minutes: 0,
+    minutes: 25,
     seconds: 0,
     timerType: "study",
     timerStatus: 0 //0 = timer off, 1 = timer on
 }
+
+
+
 //toggle timer button logic
 function toggle(){
+    
     debugger
     //get the mode attribute of the start/stop button
     let mode = memory.timerStatus
     //if the timer is off when pressed
     if(!mode){
+        //show mode in DOM
+        let status = document.getElementById("timer-mode")
+        status.innerText = memory.timerType.toUpperCase()
         //set the timer status to active
         memory.timerStatus = 1
         //make the button read as STOP
         button.innerHTML = "PAUSE"
         button.setAttribute("mode","pause")
         //if the active timer type is study
+        mode.innerText = memory.timerType
         if(memory.timerType === "study"){
             //start the timer (again)
             return initiateStudy()
@@ -74,59 +85,53 @@ function initiateStudy(){
     memory.minutes = memory.minutes
     minutes.innerHTML = memory.minutes
     studyTimer = setInterval(()=>{
-        activeTimerType = "study"
-        if(memory.minutes<25){
-            if(memory.seconds == 60){
-                memory.seconds = 0
-                memory.minutes++
-                minutes.innerHTML = memory.minutes
-            }
-            if(memory.seconds<10){
-                seconds.innerHTML = '0' + memory.seconds
-            }else{
-                seconds.innerHTML = memory.seconds
-            }
-            memory.seconds++
-        }else{
-            clearInterval(studyTimer)
-            document.dispatchEvent(add_tomato)
-            document.dispatchEvent(timer_end)
-        }
+    if(memory.minutes==0&&memory.seconds==0){
+        debugger
+        seconds.innerHTML = '0' + memory.seconds
+        document.dispatchEvent(add_tomato)
+        document.dispatchEvent(timer_end)
+        return clearInterval(studyTimer)
+    }
+    if(memory.seconds == 0){
+        memory.seconds = 59
+        memory.minutes--
+        minutes.innerHTML = memory.minutes
+    }
+    if(memory.seconds<10){
+        seconds.innerHTML = '0' + memory.seconds
+    }else{
+        seconds.innerHTML = memory.seconds
+    }
+    memory.seconds--
     },5)
 } 
 
 //increase the timer every second until 5 minutes
 function initiateBreak(){
-    clearInterval(studyTimer)
-    
-    // memory.minutes = 0
-    // memory.seconds = 0
     seconds.innerHTML = memory.seconds
     minutes.innerHTML = memory.minutes
     fiveminutebreak = setInterval(()=>{
-        if(memory.minutes<5){
-            if(memory.seconds == 60){
-                memory.seconds = 0
-                memory.minutes++
-                minutes.innerHTML = memory.minutes
-            }
-            if(memory.seconds<10){
-                seconds.innerHTML = '0' + memory.seconds
-            }else{
-                seconds.innerHTML = memory.seconds
-            }
-            memory.seconds++
-        }else{
-            clearInterval(fiveminutebreak)
+        if(memory.minutes===0&&memory.seconds==0){
+            seconds.innerHTML = '0' + memory.seconds
             document.dispatchEvent(timer_end)
+            return clearInterval(fiveminutebreak)
         }
+        if(memory.seconds == 0){
+            memory.seconds = 59
+            memory.minutes--
+            minutes.innerHTML = memory.minutes
+        }
+        if(memory.seconds<10){
+            seconds.innerHTML = '0' + memory.seconds
+        }else{
+            seconds.innerHTML = memory.seconds
+        }
+        memory.seconds--
     },5)
 } 
 
 document.addEventListener("timer_end", ()=>{
     debugger
-    memory.minutes = 0
-    memory.seconds = 0
     playSound()
     let modaltext = document.getElementById("modal-notification")
     if(memory.timerType === "study"){
@@ -140,9 +145,15 @@ document.addEventListener("timer_end", ()=>{
     modalbutton.onclick = ()=>{modal.style.display = "none"}
 
     if(memory.timerType === "break"){
+        clearInterval(studyTimer)
         memory.timerType = "study"
+        memory.minutes = 25
+        memory.seconds = 0
     }else{
+        clearInterval(fiveminutebreak)
         memory.timerType = "break"
+        memory.minutes = 5
+        memory.seconds = 0
     }
     if(round>3){
         console.log(round)
@@ -151,6 +162,8 @@ document.addEventListener("timer_end", ()=>{
     memory.timerStatus = 0
     button.innerHTML = "START"
     button.setAttribute("mode","start")
+
+    
     // if(activeTimerType ==="study"){
     //     round++
     //     console.log(round)
